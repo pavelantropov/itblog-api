20db0c59f855a4d51a95eb20f9cfbc382db2de8d
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-03-10 21:10:59 +0400

    Setup a web API project

---
+++ src/ITBlog.Api/Program.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+	options.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Version = "v1",
+		Title = "Antropov IT Blog",
+		Description = "An educational blog by Pavel Antropov on full-stack .NET development",
+		Contact = new OpenApiContact
+		{
+			Name = "Pavel Antropov",
+			Url = new Uri("https://www.linkedin.com/in/pavelantropov/")
+		},
+	});
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+	options.SwaggerEndpoint("./swagger/v1/swagger.json", "Antropov IT Blog");
+	options.RoutePrefix = string.Empty;
+});
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+	var forecast = Enumerable.Range(1, 5).Select(index =>
+	   new WeatherForecast
+	   (
+		   DateTime.Now.AddDays(index),
+		   Random.Shared.Next(-20, 55),
+		   summaries[Random.Shared.Next(summaries.Length)]
+	   ))
+		.ToArray();
+	return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
