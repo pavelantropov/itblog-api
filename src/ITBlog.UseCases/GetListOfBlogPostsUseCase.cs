f1ac3084c59b47d717cf7e59f028a4db0c5b9b15
Pavel Antropov <p.m.antropov@gmail.com>
2022-03-13 13:10:18 +0400

    Create use cases

---
+++ src/ITBlog.UseCases/GetListOfBlogPostsUseCase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Antropov.ITBlog.DataAccess.CosmosDB.Abstractions;
+using Antropov.ITBlog.UseCases.Abstractions;
+using Antropov.ITBlog.UseCases.Dto;
+
+namespace Antropov.ITBlog.UseCases;
+
+public class GetListOfBlogPostsUseCase : IGetListOfBlogPostsUseCase
+{
+	#region props
+
+	private readonly IBlogPostDataAccessObject _blogPostDao;
+
+	#endregion
+
+	#region ctors
+
+	public GetListOfBlogPostsUseCase(
+		IBlogPostDataAccessObject blogPostDao
+		)
+	{
+		_blogPostDao = blogPostDao;
+	}
+
+	#endregion
+
+	public async Task<BlogPostsDto> Invoke(
+		string title,
+		CancellationToken cancellationToken)
+	{
+		var blogPosts = await _blogPostDao.GetBlogPosts(
+			q =>  
+				q.Where(bp => bp.Title.Contains(title, StringComparison.InvariantCultureIgnoreCase)),
+			cancellationToken);
+
+		var totalCount = blogPosts.Count;
+
+		return new BlogPostsDto
+		{
+			BlogPosts = blogPosts.ToArray(),
+			BlogPostsCount = totalCount
+		};
+	}
+}
